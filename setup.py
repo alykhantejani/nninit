3938affcb078028b527bbbb585e566471d8f4007
Alykhan Tejani <alykhan.tejani@gmail.com>
2017-02-19 17:54:27 +0000

    revert back to distutils for setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(name="nninit",
       author="Alykhan Tejani",
       author_email="alykhan.tejani@gmail.com",
       description="initialization schemes for the PyTorch nn package",
-      version="0.1.dev1",
+      version="0.1.dev2",
       url="https://github.com/alykhantejani/nninit",
       py_modules=["nninit"],
       install_requires=['torch', 'numpy', 'scipy'])
